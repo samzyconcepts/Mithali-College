1e651b821aea33419ef0d3b132b1aa1978fb21ce
samzyconcepts <samzyolufemi@gmail.com>
2022-07-13 15:38:47 +0100

    working on navigation responsiveness

--- app.js
+++ app.js
@@ -9,3 +9,12 @@ function fixNav() {
     nav.classList.remove("active");
   }
 }
+
+// hamburger menu
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
